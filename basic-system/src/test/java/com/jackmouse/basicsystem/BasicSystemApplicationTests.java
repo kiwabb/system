8f81716486e5405986c1689b56f2af1487e2b3e2
周江尧 <1310179240@qq.com>
2024-11-19 10:51:56 +0800

    feat[基础系统模块]: 初始化提交

---
+++ basic-system/src/test/java/com/jackmouse/basicsystem/BasicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackmouse.basicsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
