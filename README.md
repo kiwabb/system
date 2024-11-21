2f351fa85370941d324f458a301a3984b67bf3cf
周江尧 <1310179240@qq.com>
2024-11-21 16:50:02 +0800

    feat[系统搭建]: 集成gateway和nacos

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # system
 微服务系统管理
+
+# 1.0.0
+集成gateway和nacos
--- basic-system/src/main/java/com/jackmouse/basicsystem/BasicSystemApplication.java
+++ basic-system/src/main/java/com/jackmouse/basicsystem/BasicSystemApplication.java
@@ -1,9 +1,13 @@
 package com.jackmouse.basicsystem;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@MapperScan("com.jackmouse.basicsystem.mapper")
+@EnableDiscoveryClient
 public class BasicSystemApplication {
 
     public static void main(String[] args) {
